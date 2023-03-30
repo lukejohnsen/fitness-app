7087bec7e5aebb704e00d55db4972fbd38af152b
lukejohnsen <johnsen.luke@gmail.com>
2023-03-29 19:43:16 -0700

    setting up db

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,27 @@
-require('dotenv').config();
-const express = require('express');
-const workoutRoutes = require('./routes/workouts');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 
-app.use ((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
-app.use('/api/workouts', workoutRoutes);
+app.use("/api/workouts", workoutRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server now on port ${PORT}!`);
-});
+// Connect to database
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Connected to DB. Server now on port ${PORT}!`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
